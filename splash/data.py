8ee0831ea85954ab930572f6d6005ba202c220bf
Joshua Avery <javery@yami.localdomain>
2019-07-24 05:57:38 -0700

    refined pagination

--- splash/data.py
+++ splash/data.py
@@ -47,12 +47,12 @@ class MongoCollectionDao(Dao):
         if query is None:
             # Calculate number of documents to skip
             skips = page_size * (page - 1)
-
+            num_results = self.collection.find().count()
             # Skip and limit
             cursor = self.collection.find().skip(skips).limit(page_size)
 
             # Return documents
-            return cursor
+            return  num_results, cursor
 
         else:
             raise NotImplementedError
