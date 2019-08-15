87413ccc55e2178ad849f6324c9f4714f7d191e0
Dylan McReynolds <dmcreynolds@lbl.gov>
2019-08-15 15:46:39 -0700

    Removed unused code. Merged changes for pagination.

--- test/import_experiments.py
+++
@@ -1,6 +0,0 @@
-import requests
-
-url = 'http://localhost:5000/api/experiments'
-headers = {'Accept' : 'application/json', 'Content-Type' : 'application/json'}
-r = requests.post(url, data=open('./splash-server/schema/fake_experiments/project0.json', 'rb'), headers=headers)
-print (r)
