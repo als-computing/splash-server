e00d847a147cb804a56ea7aa62d217e18e50b683
Joshua Avery <joshuaaver01@gmail.com>
2020-04-27 09:19:57 -0700

    added JSON schema, errors return Json

--- splash/__init__.py
+++ splash/__init__.py
@@ -7,6 +7,7 @@ import os
 import sys
 from splash.data.base import ObjectNotFoundError, BadIdError
 from splash.auth.oauth_resources import OauthVerificationError
+from json import dumps
 
 
 def create_app():
@@ -56,17 +57,17 @@ def create_app():
     @app.errorhandler(404)
     def resource_not_found(error):
         logger.info("Resource not found: ", exc_info=1)
-        return make_response(str({'error': 'resource_not_found', 'message':'resource not found'}), 404)
+        return make_response(dumps({"error": "resource_not_found", "message":"resource not found"}), 404)
 
     @app.errorhandler(jsonschema.exceptions.ValidationError)
     def validation_error(error):
         logger.info(" Validation Error: ", exc_info=1 )
-        return make_response(str({"error": "validation_error", 'message':str(error)}), 400)
+        return make_response(dumps({"error": "validation_error", "message":str(error)}), 400)
 
     @app.errorhandler(TypeError)
     def type_error(error):
         logger.info(" TypeError ", exc_info=1)
-        return make_response(str({"error":"type_error", "message": str(error)}), 400)
+        return make_response(dumps({"error":"type_error", "message": "Type Error"}), 400)
 
     # This actually might never get called because trailing slashes with
     # no parameters won't get routed to the route that would raise a
@@ -75,32 +76,32 @@ def create_app():
     @app.errorhandler(NoIdProvidedError)
     def no_id_provided_error(error):
         logger.info("No Id Provided Error: ", exc_info=1)
-        return make_response(str({'error':'no_id_provided','message': 'no id provided'}), 400)
+        return make_response(dumps({"error":"no_id_provided","message": "no id provided"}), 400)
 
     @app.errorhandler(ObjectNotFoundError)
     def object_not_found_error(error):
         logger.info(" Object Not Found Error: ", exc_info=1 )
-        return make_response(str({'error': 'object_not_found', 'message':'object not found'}), 404)
+        return make_response(dumps({"error": "object_not_found", "message":"object not found"}), 404)
 
     @app.errorhandler(BadIdError)
     def bad_id_error(error):
         logger.info(" Bad ID error: ", exc_info=1)
-        return make_response(str({"error": "bad_id_error", "message": "bad id error"}), 400)
+        return make_response(dumps({"error": "bad_id_error", "message": "bad id error"}), 400)
 
     @app.errorhandler(ValueError)
     def value_error(error):
         logger.info(" ValueError ", exc_info=1)
-        return make_response(str({"error": "value_error", 'message': str(error)}), 400)
+        return make_response(dumps({"error": "value_error", "message": "Value Error"}), 400)
     
     @app.errorhandler(OauthVerificationError)
     def value_error(error):
         logger.info(" OauthVerificationError ", exc_info=1)
-        return make_response(str({"error":"oauth_verification_error", 'message': "oauth verification error"}), 500)
+        return make_response(dumps({"error":"oauth_verification_error", "message": "oauth verification error"}), 500)
 
     @app.errorhandler(Exception)
     def general_error(error):
         logger.critical(" Houston we have a problem: ", exc_info=1)
-        return make_response(str({"error":"server_error", 'message': "oops, something went wrong on our end"}, 500))
+        return make_response(dumps({"error":"server_error", "message": "oops, something went wrong on our end"}, 500))
     return app
 
 
