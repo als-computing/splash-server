9f54a9a6162f455715c7c72b53bb1ea69283490d
Dylan McReynolds <dmcreynolds@lbl.gov>
2020-05-21 12:12:54 -0700

    use dotenv

--- config.py
+++ config.py
@@ -1,6 +1,11 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
 DEBUG = False
 TESTING = False
 TEST = 'from root config.py'
-MONGO_URL = 'mongodb://db:27017'
+MONGO_URL = os.getenv('MONGO_DB_URL')
 MONGO_DB_NAME = 'splash'
 REMEMBER_COOKIE_PATH = '/api'
