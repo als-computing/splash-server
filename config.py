d2dcc5c2d1052f69cd61e1939df79f73f17b49a4
Dylan McReynolds <dmcreynolds@lbl.gov>
2020-05-21 11:22:37 -0700

    undo mongo_url change

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 DEBUG = False
 TESTING = False
 TEST = 'from root config.py'
-MONGO_URL = 'mongodb://localhost:27017'
+MONGO_URL = 'mongodb://db:27017'
 MONGO_DB_NAME = 'splash'
 REMEMBER_COOKIE_PATH = '/api'
